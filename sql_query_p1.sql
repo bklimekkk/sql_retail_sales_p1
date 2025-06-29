5b1fd5d7cebbd6357ec26da0a6a6becd9f011871
Bartosz Klimek <73422213+bklimekkk@users.noreply.github.com>
2025-06-29 18:39:24 +0200

    Update sql_query_p1.sql

--- sql_query_p1.sql
+++ sql_query_p1.sql
@@ -1,3 +1,18 @@
+create table if not exists retail_sales
+(
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,	
+    sale_time TIME,
+    customer_id INT,	
+    gender VARCHAR(10),
+    age INT,
+    category VARCHAR(35),
+    quantity INT,
+    price_per_unit FLOAT,	
+    cogs FLOAT,
+    total_sale FLOAT
+);
+
 select * from retail_sales;
 
 select count (*) from retail_sales;
